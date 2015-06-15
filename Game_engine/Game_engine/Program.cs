545da0a38b37ba8f88c3af14ced70f8029314729
haloguysm1th <thomastkrie@gmail.com>
2015-06-14 22:18:26 -0500

    Intro
    Just wrote the intro

--- Game_engine/Game_engine/Program.cs
+++ Game_engine/Game_engine/Program.cs
@@ -6,7 +6,28 @@ namespace Game_engine
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("Made by Strand In Space Games");
+			intro ();
+			Console.WriteLine ("Engine by Halo and Zac");
+			intro ();
+			Console.WriteLine ("Story by Zac and Halo");
+			intro ();
+			Console.WriteLine ("Works best on anything with anything with a cpu and a screen, with a working keyboard, oh and needs c# and windows...");
+			intro ();
+			Console.WriteLine ("Check GitHub /haloguysm1th/strandinspace_games for the code");
+			intro ();
+			Console.ReadKey ();
+		}
+		public static void intro (){
+			double i = 0;
+			int b = 0;
+			while(i <100000000){
+				while (b < 30) {
+					Console.WriteLine ();
+					b++;
+				}
+				i = Convert.ToDouble (i + .1);
+			}
 		}
 	}
 }
